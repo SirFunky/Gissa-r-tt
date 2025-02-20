0e2615b9d37137a50b3026f49735443f3d279445
Niklas Karlsson <shadowblade83@hotmail.com>
2025-02-20 19:53:38 +0100

    Add project files.

---
+++ Gissa rätt/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Gissa_rätt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gissa ett tal mellan 1 och 100");
+            String gissa = Console.ReadLine();
+            int tal = Convert.ToInt32(gissa);
+            Random randomerare = new Random();
+            int slump_tal = randomerare.Next(1, 101); // här skapar vi slumptalet
+            int counter = 1;
+            while (tal != slump_tal)
+                {
+                counter++; 
+                if (tal < slump_tal) 
+                { Console.WriteLine("Du gissade för lågt.");
+                    gissa = Console.ReadLine();
+                    tal = Convert.ToInt32(gissa);
+                } //om du gissar för högt får du detta och en ny chans att gissa
+
+                else if (tal > slump_tal) 
+                { Console.WriteLine("Du gissade för högt.");
+                    gissa = Console.ReadLine();
+                    tal = Convert.ToInt32(gissa);
+                } //om du gissar för högt får du detta och en ny chans att gissa
+
+
+
+            }
+            Console.WriteLine("Grattis! du gissade rätt! Du gissade" +" "+ counter +" "+ "gånger");
+            Console.ReadLine();
+        }
+    }
+}
